6284e5f0398dfdf10b95df1aba9ca2a1eb857c3d
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-07-25 22:35:59 +0200

    move production code to core ns

--- src/greeting_kata/core.clj
+++ src/greeting_kata/core.clj
@@ -1,2 +1,29 @@
 (ns greeting-kata.core
-  (:gen-class))
+  (:require [clojure.string :as str]))
+
+(defn join-names [names]
+  (if (> (count names) 2)
+    (str/join ", and " [(str/join ", " (butlast names)) (last names)])
+    (str/join " and " names)))
+
+(defn uppercase? [word]
+  (= word (str/upper-case word)))
+
+(defn split [names]
+  [(filter (complement uppercase?) names)
+   (filter uppercase? names)])
+
+(defn salute [names]
+  (str "Hello, " names "."))
+
+(defn shout [names]
+  (str "HELLO " names "!"))
+
+(defn greet [names]
+  (if (nil? names)
+    (salute "my friend")
+    (let [names-list (if (sequential? names) names [names])
+          [lower upper] (split names-list)
+          salutations (if (empty? lower) [] [(salute (join-names lower))])
+          shouts (if (empty? upper) [] [(shout (join-names upper))])]
+      (str/join " AND " (concat salutations shouts)))))
--- test/greeting_kata/core_test.clj
+++ test/greeting_kata/core_test.clj
@@ -1,34 +1,6 @@
 (ns greeting-kata.core-test
   (:require [clojure.test :refer :all]
-            [clojure.string :as str]
             [greeting-kata.core :refer :all]))
-
-(defn join-names [names]
-  (if (> (count names) 2)
-    (str/join ", and " [(str/join ", " (butlast names)) (last names)])
-    (str/join " and " names)))
-
-(defn uppercase? [word]
-  (= word (str/upper-case word)))
-
-(defn split [names]
-  [(filter (complement uppercase?) names)
-   (filter uppercase? names)])
-
-(defn salute [names]
-  (str "Hello, " names "."))
-
-(defn shout [names]
-  (str "HELLO " names "!"))
-
-(defn greet [names]
-  (if (nil? names)
-    (salute "my friend")
-    (let [names-list (if (sequential? names) names [names])
-        [lower upper] (split names-list)
-        salutations (if (empty? lower) [] [(salute (join-names lower))])
-        shouts (if (empty? upper) [] [(shout (join-names upper))])]
-    (str/join " AND " (concat salutations shouts)))))
 
 (deftest test-greet
   (testing "Greets name"
