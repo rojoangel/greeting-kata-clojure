8fb031b2628653be28c2d7d3511fdb35a9ab41e4
Angel Rojo Marijuan <arojomarijuan@magento.com>
2017-07-25 19:52:13 +0200

    handles nulls

--- test/greeting_kata/core_test.clj
+++ test/greeting_kata/core_test.clj
@@ -3,10 +3,14 @@
             [greeting-kata.core :refer :all]))
 
 (defn greet [name]
-  (str "Hello, " name "."))
+  (if (nil? name)
+    "Hello, my friend."
+    (str "Hello, " name ".")))
 
 
 (deftest test-greet
   (testing "Greets name"
     (is (= "Hello, Paco." (greet "Paco")))
-    (is (= "Hello, Josefina." (greet "Josefina")))))
+    (is (= "Hello, Josefina." (greet "Josefina"))))
+  (testing "Handles nulls"
+    (is (= "Hello, my friend." (greet nil)))))
